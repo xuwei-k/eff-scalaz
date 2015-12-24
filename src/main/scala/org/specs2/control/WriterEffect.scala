3207b0a75554ee8f54c5563a9def7e4704a34256
Eric Torreborre <etorreborre@yahoo.com>
2015-12-24 16:31:05 +1100

    use a list buffer to accumulate values in a Writer

--- src/main/scala/org/specs2/control/WriterEffect.scala
+++ src/main/scala/org/specs2/control/WriterEffect.scala
@@ -4,30 +4,45 @@ import Eff._
 import Effects._
 import Member._
 import Interpret._
-import scalaz._
+import scalaz._, Scalaz._
+import collection.mutable._
 
 /**
  * Effect for logging values alongside computations
+ *
+ * Compared to traditional Writer monad which accumulates values by default
+ * this effect can be interpreted in different ways:
+ *
+ *  - log values to the console or to a file as soon as they are produced
+ *  - accumulate values in a list
  */
 object WriterEffect {
 
-  def write[O](o: O): Writer[O, Unit] =
-    Writer(o, ())
-
+  /** write a give value */
   def tell[R, O](o: O)(implicit member: Member[Writer[O, ?], R]): Eff[R, Unit] =
-    send[Writer[O, ?], R, Unit](write(o))
+    send[Writer[O, ?], R, Unit](Writer(o, ()))
 
+  /**
+   * run a writer effect and return the list of written values
+   *
+   * This uses a ListBuffer internally to append values
+   */
   def runWriter[R <: Effects, O, A](w: Eff[Writer[O, ?] |: R, A]): Eff[R, (A, List[O])] = {
-    val recurse: StateRecurse[Writer[O, ?], A, (A, List[O])] = new StateRecurse[Writer[O, ?], A, (A, List[O])] {
-      type S = List[O]
-      val init = List[O]()
-      def apply[X](x: Writer[O, X], l: List[O]) = (x.run._2, l :+ x.run._1)
-      def finalize(a: A, l: List[O]) = (a, l)
+    val recurse: StateRecurse[Writer[O, ?], A, (A, ListBuffer[O])] = new StateRecurse[Writer[O, ?], A, (A, ListBuffer[O])] {
+      type S = ListBuffer[O]
+      val init = new ListBuffer[O]
+      def apply[X](x: Writer[O, X], l: ListBuffer[O]) = (x.run._2, l :+ x.run._1)
+      def finalize(a: A, l: ListBuffer[O]) = (a, l)
     }
 
-    interpretState1[R, Writer[O, ?], A, (A, List[O])]((a: A) => (a, List[O]()))(recurse)(w)
+    interpretState1[R, Writer[O, ?], A, (A, ListBuffer[O])]((a: A) => (a, new ListBuffer[O]))(recurse)(w).map {
+      case (a, l) => (a, l.toList)
+    }
   }
 
+  /**
+   * run a tagged writer effect
+   */
   def runTaggedWriter[R <: Effects, T, O, A](w: Eff[({type l[X] = Writer[O, X] @@ T})#l |: R, A]): Eff[R, (A, Vector[O])] = {
     type W[X] = Writer[O, X] @@ T
 
